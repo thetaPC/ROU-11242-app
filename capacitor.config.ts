e51e83cc28ab4f2c2f5689123b8149f5e2b2136c
Maria Hutt <maria@ionic.io>
2024-10-23 08:23:07 -0700

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ROU-11242-app',
+  webDir: 'dist'
+};
+
+export default config;
